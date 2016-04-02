d58267c808a3f4da521c3b22b9f88450d9fe2b6c
Piotr Grudzien <piotrgrudzien@MacBook-Pro-Piotrka.local>
2016-04-02 22:15:36 +0100

    Gazeta finished

--- GazetaCrawl.py
+++ GazetaCrawl.py
@@ -7,8 +7,8 @@ Created on Sat Apr  2 19:29:22 2016
 
 from bs4 import BeautifulSoup, Comment
 from Utils import removePunct
-import urllib2
-import time, datetime, string
+import urllib2, pickle
+import time, datetime
 
 link = 'http://wiadomosci.gazeta.pl/wiadomosci/0,0.html'
 
@@ -19,34 +19,45 @@ soup = BeautifulSoup(r, 'lxml')
 """ Find the sections where all the links are """
 section = soup.find(id = 'holder_201')
 
-""" Get the links of all the actual articles """
-Gazeta_Timeline = {}
+""" Load the timeline from file """
+try:
+    Gazeta_Timeline = pickle.load(open('../Gazeta/Gazeta_Timeline.p', 'rb'))
+except IOError:
+    print 'Loading empty Gazeta_Timeline'
+    Gazeta_Timeline = {}
+    
 Gazeta_Urls = []
+
+""" Get the links of the articles you haven't seen before """
 for link in section.find_all('a'):
     newUrl = link.get('href')
     if(newUrl.startswith('http://wiadomosci.gazeta.pl/wiadomosci/') and (newUrl.endswith('.html')) and (newUrl not in Gazeta_Timeline)):
         Gazeta_Timeline[newUrl] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         Gazeta_Urls.append(newUrl)
         
-r = urllib2.urlopen(Gazeta_Urls[2]).read()
-soup = BeautifulSoup(r, 'lxml')
-print 'Working with link', Gazeta_Urls[2]
+""" Pickle the updated timeline """
+pickle.dump(Gazeta_Timeline, open("../Gazeta/Gazeta_Timeline.p", "wb"))
+   
+""" Get text of each article and write to file """
+print 'Adding', len(Gazeta_Urls), 'articles'
+for url in Gazeta_Urls:
+    r = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(r, 'lxml')
+    print 'Link:', url
+    title = soup.title.string
+    
+    f = open('../Gazeta/Articles/' + title + '.txt', 'w')
 
-f = open('../Output.txt', 'w')
-title = soup.title.string
-f.write(removePunct(title.encode('UTF8')) + '\n')
-print 'Title written to file'
-
-bold = soup.find(id = 'gazeta_article_lead').get_text()
-f.write(removePunct(bold.encode('UTF8')) + '\n')
-print 'Bold paragraph written to file'
-
-body = soup.find(id = 'artykul')
-
-for item in body.findAll(text = True, recursive = False):
-    """ Ignore comments and empty strings """
-    if((not isinstance(item, Comment)) or (not item)):
-        out = removePunct(item.encode('UTF8'))
-        f.write(out)
-print 'Article body written to file'
-f.close()
+    f.write(removePunct(title.encode('UTF8')) + '\n')
+    
+    bold = soup.find(id = 'gazeta_article_lead').get_text()
+    f.write(removePunct(bold.encode('UTF8')) + '\n')
+    
+    body = soup.find(id = 'artykul')
+    
+    for item in body.findAll(text = True, recursive = False):
+        """ Ignore comments and empty strings """
+        if((not isinstance(item, Comment)) or (not item)):
+            out = removePunct(item.encode('UTF8'))
+            f.write(out)
+    f.close()
