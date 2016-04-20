d88be38f969143e861f4903efd6b8a1a7d8b78d6
Piotr Grudzien <piotrgrudzien@MacBook-Pro-Piotrka.local>
2016-04-20 23:06:45 +0100

    W test.py juz mam liste linkow do wPolityce

--- test.py
+++ test.py
@@ -15,44 +15,10 @@ def is_number(s):
 from bs4 import BeautifulSoup
 import urllib2
 
-link = 'http://niezalezna.pl/'
+link = 'http://wpolityce.pl/'
 
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-response = opener.open(link)
+r = urllib2.urlopen(link).read()
+soup = BeautifulSoup(r, 'lxml')
 
-soup = BeautifulSoup(response, 'lxml')
-
-Niezalezna_Urls = []
-
-for l in soup.find_all('a'):
-    newUrl = l.get('href')
-    if(newUrl.startswith(link) & (newUrl != link)):
-        Niezalezna_Urls.append(l.get('href'))
-    elif((newUrl.startswith('/')) & (is_number(newUrl[1:3]))):
-        Niezalezna_Urls.append(link + newUrl[1:])
-        
-Niezalezna_Urls = list(set(Niezalezna_Urls))
-        
-#for link in Niezalezna_Urls:
-#    print link
-    
-#print Niezalezna_Urls[0]
-    
-print 'http://niezalezna.pl/79263-gwizdy-na-lecha'
-
-response = opener.open('http://niezalezna.pl/79263-gwizdy-na-lecha')
-
-soup = BeautifulSoup(response, 'lxml')
-
-#print soup.prettify()
-
-title = soup.find(id = 'content').h1.text
-print title
-
-#bold = soup.find(id = 'content').strong.text
-#print bold
-#
-#body = soup.find("div", {"class":"node-content"})
-#print 'TEXT'
-#print ''.join(soup.find("div", {"class":"node-content"}).findAll(text = True, recursive = False)).encode('UTF8')
+for article in soup.find_all('article'):
+    print article.find('a')['href']
